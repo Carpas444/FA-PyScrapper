53d6b7b2fa2caf37db07474d0ce9cdd817d1f58a
a106909 <a106909@alunos.uminho.pt>
2025-04-29 22:27:19 +0100

    Minor changes - cleaned code

--- scrapper.py
+++ scrapper.py
@@ -41,6 +41,9 @@ path = '/home/afonso/Downloads/chromedriver-linux64/chromedriver'
 service = Service(executable_path = path)
 options = Options()
 options.add_argument("--headless")
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--disable-gpu')
 driver = webdriver.Chrome(service=service, options=options)
 
 #Espera até que a tabela esteja carregada no site
@@ -92,40 +95,51 @@ try:
 except Exception as e:
         logging.error(f"Ocorreu um erro: {str(e)}", exc_info=True)
 
-row_to_write = len(sheet.col_values(1)) + 1 #Encontra a última linha com dados escritas na coluna A e adiciona 1 a essa linha (i.e. linha onde pretendemos escrever, linha a baixo da linha atualmente escrita)
+row_to_write = len(sheet.col_values(1)) + 1 #Encontra a última linha com dados escritas na coluna A e adiciona 1 a essa linha (i.e. linha onde pretendemos escrever, linha abaixo da linha atualmente escrita)
 
-
-def formatar_para_duas_casas(numero): #Formata um número para que tenha sempre duas casas inteiras (2 => 02; 14 => 14)
+# Formata um número para que tenha sempre duas casas inteiras (2 => 02; 14 => 14)
+def formatar_para_duas_casas(numero):
     return "{:02d}".format(numero)
 
-dataData = datetime.datetime.now() #Pega na data atual. The problem is, se fizermos data.day ou data.month, por exemplo, isso vem como int e eu pretendo uma string, daí na linha imediatamente a baixo, estar sempre a converter cada valor
-dataDiaEhora = str(formatar_para_duas_casas(dataData.day)) + '/' + str(formatar_para_duas_casas(dataData.month)) + '/' + str(dataData.year) + ' - ' + str(formatar_para_duas_casas(dataData.hour)) + ':' + str(formatar_para_duas_casas(dataData.minute))
-range_to_write_new_date = f'A{row_to_write}:A{row_to_write}' #Células (neste caso, apenas uma) onde se pretende escrever a data atual
-try:
-    sheet.update(range_name=range_to_write_new_date, values=[[dataDiaEhora]]) #Escreve na célula pretendida a data atual (tem de ser array 2D, pois é o que o values está à espera de receber!!)
-    logging.info(f"Escreveu a data atual na coluna A da linha {row_to_write}!")
-except Exception as e:
-        logging.error(f"Ocorreu um erro: {str(e)}", exc_info=True)
+# Escreve a data atual na primeira coluna
+def escreve_data_atual():
+    dataData = datetime.datetime.now() #Pega na data atual. The problem is, se fizermos data.day ou data.month, por exemplo, isso vem como int e eu pretendo uma string, daí na linha imediatamente a baixo, estar sempre a converter cada valor
+    dataDiaEhora = str(formatar_para_duas_casas(dataData.day)) + '/' + str(formatar_para_duas_casas(dataData.month)) + '/' + str(dataData.year) + ' - ' + str(formatar_para_duas_casas(dataData.hour)) + ':' + str(formatar_para_duas_casas(dataData.minute))
+    range_to_write_new_date = f'A{row_to_write}:A{row_to_write}' #Células (neste caso, apenas uma) onde se pretende escrever a data atual
+    try:
+        sheet.update(range_name=range_to_write_new_date, values=[[dataDiaEhora]]) #Escreve na célula pretendida a data atual (tem de ser array 2D, pois é o que o values está à espera de receber!!)
+        logging.info(f"Escreveu a data atual na coluna A da linha {row_to_write}!")
+    except Exception as e:
+            logging.error(f"Ocorreu um erro: {str(e)}", exc_info=True)
+
 
 # Escreve os dados na sheet, começando na coluna B e acabando na coluna I
-range_to_write = f'B{row_to_write}:I{row_to_write}'
-try:
-    sheet.update(range_name = range_to_write, values=[dataToWrite])
-    logging.info(f"Escreveu os valores nas colunas da linha {row_to_write}!")
-except Exception as e:
-        logging.error(f"Ocorreu um erro: {str(e)}", exc_info=True)
+def escreve_dados_scrapped():
+    range_to_write = f'B{row_to_write}:I{row_to_write}'
+    try:
+        sheet.update(range_name = range_to_write, values=[dataToWrite])
+        logging.info(f"Escreveu os valores nas colunas da linha {row_to_write}!")
+    except Exception as e:
+            logging.error(f"Ocorreu um erro: {str(e)}", exc_info=True)
 
-valorDoDiaAnteriorAceites = int(sheet.acell(f'B{row_to_write - 1}').value) #Calula o número de candidaturas aceites no dia anterior
-diferencaDiariaAceites = int(dataToWrite[0]) - valorDoDiaAnteriorAceites #Calcula a diferença do número de candidaturas aceites entre o dia atual e o anterior
-diferencaDiariaAceitesSTR = str(diferencaDiariaAceites) #Apenas transforma o valor inteiro numa string para poder concatenar com texto na linha imediatamente abaixo
-text_to_write_in_last_cell = "Mais " + diferencaDiariaAceitesSTR + " aceites" #Concatenação do texto com a diferença de valores
+# Escreve a diferença de valores entre dois dias consecutivos (na verdade, é entre a última célula e a atual - podem ter sido feitas várias run no mesmo dia - didnt feel like changing the name of the function and variables tho)
+def escreve_diferenca():
+    valorDoDiaAnteriorAceites = int(sheet.acell(f'B{row_to_write - 1}').value) #Calula o número de candidaturas aceites no dia anterior
+    diferencaDiariaAceites = int(dataToWrite[0]) - valorDoDiaAnteriorAceites #Calcula a diferença do número de candidaturas aceites entre o dia atual e o anterior
+    diferencaDiariaAceitesSTR = str(diferencaDiariaAceites) #Apenas transforma o valor inteiro numa string para poder concatenar com texto na linha imediatamente abaixo
+    text_to_write_in_last_cell = "Mais " + diferencaDiariaAceitesSTR + " aceites" #Concatenação do texto com a diferença de valores
 
-range_to_write_diff_accepted = f'J{row_to_write}:J{row_to_write}' #Células (neste caso, apenas uma) onde se pretende escrever o text_to_write_in_last_cell
-try:
-    sheet.update(range_name = range_to_write_diff_accepted, values = [[text_to_write_in_last_cell]]) #Escreve na célula pretendida o text_to_write_in_last_cell
-    logging.info(f"Escreveu a diferença de valores ({diferencaDiariaAceitesSTR}) na célula J da linha {row_to_write}!")
-except Exception as e:
-        logging.error(f"Ocorreu um erro: {str(e)}", exc_info=True)
+    range_to_write_diff_accepted = f'J{row_to_write}:J{row_to_write}' #Células (neste caso, apenas uma) onde se pretende escrever o text_to_write_in_last_cell
+    try:
+        sheet.update(range_name = range_to_write_diff_accepted, values = [[text_to_write_in_last_cell]]) #Escreve na célula pretendida o text_to_write_in_last_cell
+        logging.info(f"Escreveu a diferença de valores ({diferencaDiariaAceites}) na célula J da linha {row_to_write}!")
+    except Exception as e:
+            logging.error(f"Ocorreu um erro: {str(e)}", exc_info=True)
+
+
+escreve_data_atual()
+escreve_dados_scrapped()
+escreve_diferenca()
 
 
 #Habilita de novo a suspensão do computador
